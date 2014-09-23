307d9d07949770cdf4cccbf44d60a3414a54c124
Ilya <ilya_at_csu@mail.ru>
2014-09-23 10:51:20 +0400

    Button & settings class

---
+++ Com.ussr.oldscool.electronix/assets/make.bat
@@ -0,0 +1,3 @@
+rem java -cp gdx.jar;gdx-tools.jar com.badlogic.gdx.tools.texturepacker.TexturePacker ./ ./ test
+java -jar texturepacker-gui.jar
+pause
---
+++ Com.ussr.oldscool.electronix/src/com/ussr/oldscool/electronix/common/Constants.java
@@ -0,0 +1,73 @@
+package com.ussr.oldscool.electronix.common;
+
+public final class Constants {
+
+	public final class Banners {
+		
+		public final static int topOffset = 0;
+		public final static int leftOffset = 0;
+		public final static int rightOffset = 0;
+		public final static int downOffset = 0;
+		
+	}
+	
+	public final class MainScreen {
+	
+		public final static int buttonsCount = 10;
+		public final static int buttonsPerLine = 3;
+			
+	}
+	
+	public final class GameScreen {
+		
+		//gamescreen
+		public final static int dScreenLeft = 0;
+		public final static int dScreenTop = 0;
+		public final static int dScreenWidth = 0;
+		public final static int dScreenHeight = 0;
+		
+		//left upper button
+		public final static int dButton1Left = 0;
+		public final static int dButton1Right = 0;
+		public final static int dButton1Width = 0;
+		public final static int dButton1Height = 0;
+		
+		//left down button
+		public final static int dButton2Left = 0;
+		public final static int dButton2Right = 0;
+		public final static int dButton2Width = 0;
+		public final static int dButton2Height = 0;
+		
+		//right upper button
+		public final static int dButton3Left = 0;
+		public final static int dButton3Right = 0;
+		public final static int dButton3Width = 0;
+		public final static int dButton3Height = 0;
+		
+		//right down button
+		public final static int dButton4Left = 0;
+		public final static int dButton4Right = 0;
+		public final static int dButton4Width = 0;
+		public final static int dButton4Height = 0;
+		
+		//button game A
+		public final static int dButtonALeft = 0;
+		public final static int dButtonARight = 0;
+		public final static int dButtonAWidth = 0;
+		public final static int dButtonAHeight = 0;
+		
+		//button game B
+		public final static int dButtonBLeft = 0;
+		public final static int dButtonBRight = 0;
+		public final static int dButtonBWidth = 0;
+		public final static int dButtonBHeight = 0;
+		
+		//button time / settings
+		public final static int dButtonSLeft = 0;
+		public final static int dButtonSRight = 0;
+		public final static int dButtonSWidth = 0;
+		public final static int dButtonSHeight = 0;
+		
+	}
+	
+}
